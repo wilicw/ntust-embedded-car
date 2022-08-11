6008d1941d188d3e0cb55d08bc6073ec38743f90
William Chang <wilicw1346@gmail.com>
2022-08-11 16:17:47 +0800

    Implement some functions

--- driver/core/vision.cpp
+++ driver/core/vision.cpp
@@ -1,4 +1,15 @@
 #include "vision.h"
 
 void Vision::process(cv::Mat &frame, cv::Mat &final) {
+    vector<cv::Mat> channel;
+    cv::split(frame, channel);
+    cv::Mat &red_frame = channel[0];
+    cv::Mat thresh;
+
+    cv::adaptiveThreshold(red_frame, thresh, 255,
+                          cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 25,
+                          25);
+    //    cv::threshold(red_frame, thresh, 100, 255, cv::THRESH_BINARY);
+
+    final = thresh;
 }
