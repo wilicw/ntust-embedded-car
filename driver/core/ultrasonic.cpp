93f816efa7c8cd7768518f33e059a7fb7184c05b
William Chang <wilicw1346@gmail.com>
2022-08-12 11:02:33 +0800

    Lower the ultrasonic timeout

--- driver/core/ultrasonic.cpp
+++ driver/core/ultrasonic.cpp
@@ -11,17 +11,17 @@ double Ultrasonic::distance() {
     digitalWrite(this->trig_pin, LOW);
     delayMicroseconds(2);
     digitalWrite(this->trig_pin, HIGH);
-    delayMicroseconds(15);
+    delayMicroseconds(10);
     digitalWrite(this->trig_pin, LOW);
 
     unsigned int Trig_time;
     Trig_time = micros();
     while (!digitalRead(this->echo_pin))
-        if ((micros() - Trig_time) > 30000) return -1.0;
+        if ((micros() - Trig_time) > 4000) return 99999;
 
     Trig_time = micros();
     while (digitalRead(this->echo_pin))
-        if ((micros() - Trig_time) > 30000) return -1.0;
+        if ((micros() - Trig_time) > 4000) return 99999;
 
     return (micros() - Trig_time) * 340.0 / 2.0 / 10000.0;
 }
