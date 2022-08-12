52a03e2af2436abb352ff581673a54a76d6103bb
NTUST car <you@example.com>
2022-08-12 09:56:08 +0800

    modified 'final' word in vision.cpp

--- driver/core/vision.cpp
+++ driver/core/vision.cpp
@@ -1,15 +1,12 @@
 #include "vision.h"
 
-void Vision::process(cv::Mat &frame, cv::Mat &final) {
+void Vision::process(cv::Mat &frame, cv::Mat &output) {
     vector<cv::Mat> channel;
     cv::split(frame, channel);
     cv::Mat &red_frame = channel[0];
-    cv::Mat thresh;
 
-    cv::adaptiveThreshold(red_frame, thresh, 255,
+    cv::adaptiveThreshold(red_frame, output, 255,
                           cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 25,
                           25);
     //    cv::threshold(red_frame, thresh, 100, 255, cv::THRESH_BINARY);
-
-    final = thresh;
 }
