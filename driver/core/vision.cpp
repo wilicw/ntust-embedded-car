1fd09c3435a5aad8020c918218337d8d274aca51
William Chang <wilicw1346@gmail.com>
2022-08-22 18:15:42 +0800

    Implement laplacian_check and part of process

--- driver/core/vision.cpp
+++ driver/core/vision.cpp
@@ -6,7 +6,7 @@ float Vision::distance(cv::Point center) {
     constexpr double camera_near = 10.0;
 
     const double thetaEnd = atan(camera_height / camera_near);
-    const double thetaStart = thetaEnd - 0.6920252660101123; // By camera DFOV
+    const double thetaStart = thetaEnd - 0.6920252660101123;  // By camera DFOV
     double theta = thetaStart + (thetaEnd - thetaStart) * center.y / h;
 
     double thetasq = tan(theta);
@@ -24,18 +24,32 @@ float Vision::distance(cv::Point center) {
 }
 
 sign_item_t Vision::process(cv::Mat image) {
+    const static sign_item_t empty_result = {
+        .center = nullptr,
+        .cropped = nullptr,
+    };
+
     sign_item_t result = {
         .center = nullptr,
         .cropped = nullptr,
     };
 
+    if (!this->laplacian_check(image)) return empty_result;
+
+    cv::copyMakeBorder(image, image, 10, 10, 10, 10, cv::BORDER_CONSTANT);
+
     return result;
 }
 bool Vision::laplacian_check(cv::Mat image) {
-    constexpr int blurry_threshold = 60;
-    return false;
+    constexpr int blurry_threshold = 17;
+    cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_RGB2GRAY);
+    cv::Laplacian(gray, gray, CV_64F);
+    cv::Scalar mean, stddev;
+    cv::meanStdDev(gray, mean, stddev, cv::Mat());
+    double variance = stddev.val[0] * stddev.val[0];
+    return variance > blurry_threshold;
 }
 
 void Vision::contrast_normalization(cv::Mat image) {
-
 }
